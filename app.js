1f5a44ef379b9b63d6d099381425c2e36cac5d69
Justin <justincyork@gmail.com>
2016-01-07 22:36:35 +0000

    fix port binding

--- app.js
+++ app.js
@@ -41,6 +41,6 @@ server.post('/token', function (req, res, next) {
   
 });
  
-server.listen(8080, function () {
+server.listen($PORT, function () {
   console.log('%s listening at %s', server.name, server.url);
 });
